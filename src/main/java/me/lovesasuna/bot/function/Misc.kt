a56dda65a0dbc00196e1e1358f6a96edb9ee24c0
梦曦·花已落 <qq625924077@gmail.com>
2020-05-26 11:00:10 +0800

    [F] not show image

--- src/main/java/me/lovesasuna/bot/function/Misc.kt
+++ src/main/java/me/lovesasuna/bot/function/Misc.kt
@@ -35,7 +35,7 @@ class Misc : Listener {
             message.contains("怎么") -> event.reply(event.uploadImage(File(imagePath("how.jpg"))))
             message.contains("啊这") -> event.reply("这啊")
             message.contains("问题") -> event.reply("解决不了问题就解决提出问题的人")
-            message == "/donate" -> event.reply(PlainText("\"我的肚子已经菠萝菠萝哒\\n\"") + event.uploadImage(File(imagePath("pay.jpg"))))
+            message == "/donate" -> event.reply(PlainText("我的肚子已经菠萝菠萝哒\n") + event.uploadImage(File(imagePath("pay.jpg"))))
         }
         return true
     }
