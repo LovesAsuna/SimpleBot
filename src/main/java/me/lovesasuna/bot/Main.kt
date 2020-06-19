d849bb2dd457b2d6e1a1f44f91a8f49520fa60e0
梦曦·花已落 <qq625924077@gmail.com>
2020-06-19 15:31:57 +0800

    [O] 适配新的数据读入方式

--- src/main/java/me/lovesasuna/bot/Main.kt
+++ src/main/java/me/lovesasuna/bot/Main.kt
@@ -1,45 +1,37 @@
 package me.lovesasuna.bot
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import kotlinx.coroutines.runBlocking
 import me.lovesasuna.bot.data.BotData
 import me.lovesasuna.bot.file.Config
+import me.lovesasuna.bot.function.Dynamic
 import me.lovesasuna.bot.function.Notice
 import me.lovesasuna.bot.listener.FriendMessageListener
 import me.lovesasuna.bot.listener.GroupMessageListener
+import me.lovesasuna.bot.manager.FileManager
 import me.lovesasuna.bot.util.Dependence.Companion.init
-import me.lovesasuna.bot.util.SRVConvertUtil
-import net.mamoe.mirai.Bot
 import net.mamoe.mirai.console.plugins.PluginBase
-import net.mamoe.mirai.message.data.MessageChain
 import java.io.File
+import java.io.FileOutputStream
+import java.io.ObjectOutputStream
 
 /**
  * @author LovesAsuna
  * @date 2020/5/6 21:54
  */
 class Main : PluginBase() {
-    private lateinit var bot: Bot
-
-
-    @Suppress("UNCHECKED_CAST")
     override fun onEnable() {
         instance = this
         Config.init(this)
-
+        println("[Bot] 机器人插件启用成功！")
         runBlocking {
             init()
-            if (File(dataFolder.toString() + File.separator + "notice.json").exists()) {
-                BotData.objectMapper?.readValue(File(dataFolder.toString() + "notice.json"), ArrayList::class.java)?.forEach {
-                    it as Triple<Long, Long, MessageChain>
-                    Notice.msgList.add(it)
-                }
-            }
+            FileManager.readValue()
         }
     }
 
     override fun onDisable() {
-        BotData.objectMapper?.writeValue(File(dataFolder.toString() + File.separator + "notice.json"), Notice.msgList)
+        BotData.objectMapper!!.writeValue(File(dataFolder.toString() + File.separator + "notice.json"), Notice.Companion::class.java)
+        ObjectOutputStream(FileOutputStream(File(dataFolder.toString() + File.separator + "dynamic.json"))).writeObject(Dynamic.data)
     }
 
     companion object {
---
+++ src/main/java/me/lovesasuna/bot/manager/FileManager.kt
@@ -0,0 +1,27 @@
+package me.lovesasuna.bot.manager
+
+import me.lovesasuna.bot.Main
+import me.lovesasuna.bot.function.Dynamic
+import me.lovesasuna.bot.function.Notice
+import java.io.File
+import java.io.FileInputStream
+import java.io.ObjectInputStream
+
+object FileManager {
+    private val noticeFile = File(Main.instance.dataFolder.toString() + File.separator + "notice.json")
+    private val dynamicFile = File(Main.instance.dataFolder.toString() + File.separator + "dynamic.json")
+
+    fun readValue() {
+        if (noticeFile.exists()) {
+            val data = ObjectInputStream(FileInputStream(noticeFile)).readObject() as Notice.Data
+            Notice.data = data
+        }
+
+        if (dynamicFile.exists()) {
+            val data = ObjectInputStream(FileInputStream(dynamicFile)).readObject() as Dynamic.Data
+            Dynamic.data = data
+        }
+
+
+    }
+}
