beb4b1f55ae75f6acf106ded5609ca63c1403fcb
梦曦·花已落 <qq625924077@gmail.com>
2020-06-22 11:54:56 +0800

    [U] convert to kotlin

--- src/main/java/me/lovesasuna/bot/util/QueryUtil.kt
+++ src/main/java/me/lovesasuna/bot/util/QueryUtil.kt
@@ -1,129 +1,118 @@
-package me.lovesasuna.bot.util;
+package me.lovesasuna.bot.util
 
-import java.io.*;
-import java.net.Socket;
+import java.io.*
+import java.net.Socket
 
-public class QueryUtil {
-    public static String query(String host, int port) throws IOException{
-        Socket socket = new Socket(host, port);
-        socket.setSoTimeout(10 * 1000);
-        OutputStream outputStream = socket.getOutputStream();
-        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-
-        InputStream inputStream = socket.getInputStream();
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-
-        ByteArrayOutputStream b = new ByteArrayOutputStream();
-
-        DataOutputStream handshake = new DataOutputStream(b);
+object QueryUtil {
+    @Throws(IOException::class)
+    fun query(host: String, port: Int): String {
+        val socket = Socket(host, port)
+        socket.soTimeout = 10 * 1000
+        val outputStream = socket.getOutputStream()
+        val dataOutputStream = DataOutputStream(outputStream)
+        val inputStream = socket.getInputStream()
+        val inputStreamReader = InputStreamReader(inputStream)
+        val b = ByteArrayOutputStream()
+        val handshake = DataOutputStream(b)
         /*握手数据包id*/
-        handshake.writeByte(0x00);
+        handshake.writeByte(0x00)
         /*协议版本*/
-        writeVarInt(handshake, 578);
+        writeVarInt(handshake, 578)
         /*主机地址长度*/
-        writeVarInt(handshake, host.length());
+        writeVarInt(handshake, host.length)
         /*主机地址*/
-        handshake.writeBytes(host);
+        handshake.writeBytes(host)
         /*端口*/
-        handshake.writeShort(25565);
+        handshake.writeShort(25565)
         /*状态(握手是1)*/
-        writeVarInt(handshake, 1);
+        writeVarInt(handshake, 1)
 
         /*发送的握手数据包大小*/
-        writeVarInt(dataOutputStream, b.size());
+        writeVarInt(dataOutputStream, b.size())
         /*发送握手数据包*/
-        dataOutputStream.write(b.toByteArray());
+        dataOutputStream.write(b.toByteArray())
 
 
         /*大小为1*/
-        dataOutputStream.writeByte(0x01);
+        dataOutputStream.writeByte(0x01)
         /*ping的数据包id*/
-        dataOutputStream.writeByte(0x00);
-        DataInputStream dataInputStream = new DataInputStream(inputStream);
+        dataOutputStream.writeByte(0x00)
+        val dataInputStream = DataInputStream(inputStream)
         /*返回的数据包大小*/
-        int size = readVarInt(dataInputStream);
+        val size = readVarInt(dataInputStream)
         /*返回的数据包id*/
-        int id = readVarInt(dataInputStream);
-
+        var id = readVarInt(dataInputStream)
         if (id == -1) {
-            throw new IOException("数据流过早结束");
+            throw IOException("数据流过早结束")
         }
 
-        /*需要返回的状态*/
-        if (id != 0x00) {
-            throw new IOException("无效的数据包id");
+        /*需要返回的状态*/if (id != 0x00) {
+            throw IOException("无效的数据包id")
         }
         /*json字符串长度*/
-        int length = readVarInt(dataInputStream);
-
+        val length = readVarInt(dataInputStream)
         if (length == -1) {
-            throw new IOException("数据流过早结束");
+            throw IOException("数据流过早结束")
         }
-
         if (length == 0) {
-            throw new IOException("无效的json字符串长度");
+            throw IOException("无效的json字符串长度")
         }
-
-        byte[] in = new byte[length];
+        val `in` = ByteArray(length)
         /*读取json字符串*/
-        dataInputStream.readFully(in);
-        String json = new String(in, "UTF-8");
-
-
-        long now = System.currentTimeMillis();
+        dataInputStream.readFully(`in`)
+        val json = String(`in`, Charsets.UTF_8)
+        val now = System.currentTimeMillis()
         /*数据包大小*/
-        dataOutputStream.writeByte(0x09);
+        dataOutputStream.writeByte(0x09)
         /*ping 0x01*/
-        dataOutputStream.writeByte(0x01);
+        dataOutputStream.writeByte(0x01)
         /*时间*/
-        dataOutputStream.writeLong(now);
-
-        readVarInt(dataInputStream);
-        id = readVarInt(dataInputStream);
+        dataOutputStream.writeLong(now)
+        readVarInt(dataInputStream)
+        id = readVarInt(dataInputStream)
         if (id == -1) {
-            throw new IOException("数据流过早结束");
+            throw IOException("数据流过早结束")
         }
-
         if (id != 0x01) {
-            throw new IOException("无效的数据包id");
+            throw IOException("无效的数据包id")
         }
         /*读取回应*/
-        long pingtime = dataInputStream.readLong();
-
-        dataOutputStream.close();
-        outputStream.close();
-        inputStreamReader.close();
-        inputStream.close();
-        socket.close();
-
-        return json;
+        val pingtime = dataInputStream.readLong()
+        dataOutputStream.close()
+        outputStream.close()
+        inputStreamReader.close()
+        inputStream.close()
+        socket.close()
+        return json
     }
 
-    private static void writeVarInt(DataOutputStream out, int paramInt) throws IOException {
+    @Throws(IOException::class)
+    private fun writeVarInt(out: DataOutputStream, paramInt: Int) {
+        var paramInt = paramInt
         while (true) {
-            if ((paramInt & 0xFFFFFF80) == 0) {
-                out.writeByte(paramInt);
-                return;
+            if (paramInt and -0x80 == 0) {
+                out.writeByte(paramInt)
+                return
             }
-
-            out.writeByte(paramInt & 0x7F | 0x80);
-            paramInt >>>= 7;
+            out.writeByte(paramInt and 0x7F or 0x80)
+            paramInt = paramInt ushr 7
         }
     }
 
-    public static int readVarInt(DataInputStream in) throws IOException {
-        int i = 0;
-        int j = 0;
+    @Throws(IOException::class)
+    fun readVarInt(`in`: DataInputStream): Int {
+        var i = 0
+        var j = 0
         while (true) {
-            int k = in.readByte();
-            i |= (k & 0x7F) << j++ * 7;
+            val k = `in`.readByte().toInt()
+            i = i or (k and 0x7F shl j++ * 7)
             if (j > 5) {
-                throw new RuntimeException("VarInt too big");
+                throw RuntimeException("VarInt too big")
             }
-            if ((k & 0x80) != 128) {
-                break;
+            if (k and 0x80 != 128) {
+                break
             }
         }
-        return i;
+        return i
     }
 }
