ecb8533205ad0fa30aa4f29dd5cc64b758f002d5
梦曦·花已落 <qq625924077@gmail.com>
2020-07-14 19:01:54 +0800

    [Optimize] 依赖高速下载

--- src/main/java/me/lovesasuna/bot/util/network/DownloadUtil.kt
+++ src/main/java/me/lovesasuna/bot/util/network/DownloadUtil.kt
@@ -1,5 +1,6 @@
 package me.lovesasuna.bot.util.network
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
@@ -11,7 +12,14 @@ import java.net.URL
  * @date 2020/2/17 16:45
  */
 object DownloadUtil {
-    fun download(urlString: String?, fileName: String, savePath: String, vararg heads: Array<String>): Boolean {
+    /**
+     * @param urlString 下砸地址
+     * @param fileName 不包含路径的文件名
+     * @param savePath 不包含文件名的路径
+     * @param consumer 消费者函数，对下载时的字节长度进行处理
+     * @param heads 请求头
+     */
+    fun download(urlString: String?, fileName: String, savePath: String, consumer: (Int) -> Unit = {}, vararg heads: Array<String>): Boolean {
         return try {
             val url = URL(urlString)
             val conn = url.openConnection() as HttpURLConnection
@@ -22,18 +30,29 @@ object DownloadUtil {
                 }
                 connect()
             }
-            download(conn, fileName, savePath)
+            download(conn, fileName, savePath, consumer)
             true
         } catch (e: IOException) {
             false
         }
     }
 
+    /**
+     * @param conn 已经设置好属性并已经连接的HttpURLConnection
+     * @param fileName 不包含路径的文件名
+     * @param savePath 不包含文件名的路径
+     * @param consumer 消费者函数，对下载时的字节长度进行处理
+     */
     @Throws(IOException::class)
-    fun download(conn: HttpURLConnection, fileName: String, savePath: String) {
-        download(conn, File(savePath + File.separator + fileName)) {}
+    fun download(conn: HttpURLConnection, fileName: String, savePath: String, consumer: (Int) -> Unit = {}) {
+        download(conn, File(savePath + File.separator + fileName), consumer)
     }
 
+    /**
+     * @param conn 已经设置好属性并已经连接的HttpURLConnection
+     * @param file 文件绝对路径
+     * @param consumer 消费者函数，对下载时的字节长度进行处理
+     */
     @Throws(IOException::class)
     fun download(conn: HttpURLConnection, file: File, consumer: (Int) -> Unit) {
         val inputStream = conn.inputStream
@@ -46,5 +65,38 @@ object DownloadUtil {
         }
         fileOutputStream.close()
         conn.disconnect()
+    }
+
+    /**
+     * @param id 蓝奏云地址后的字符串
+     * @param fileName 不包含路径的文件名
+     * @param savePath 不包含文件名的路径
+     * @param consumer 消费者函数，对下载时的字节长度进行处理
+     */
+    fun lanzousDownload(id: String, fileName: String, savePath: String, consumer: (Int) -> Unit = {}) {
+        download(getlanzousUrl(id), fileName, savePath, consumer, arrayOf("Accept-Language", "zh-CN,zh;q=0.9"))
+    }
+
+    /**
+     * @param id 蓝奏云地址后的字符串
+     */
+    fun getlanzousUrl(id: String): String {
+        val lanzousUrl = "https://wwa.lanzous.com/$id"
+        var reader = NetWorkUtil.get(lanzousUrl)!!.first.bufferedReader()
+        for (i in 0 until 45) reader.readLine()
+        val src = reader.readLine().also { reader.close() }
+        val fn = src.split("\"")[5]
+        val fnurl = "https://wwa.lanzous.com$fn"
+        reader = NetWorkUtil.get(fnurl)!!.first.bufferedReader()
+        for (i in 0 until 18) reader.readLine()
+        val sign = reader.readLine().split("'")[1]
+        val data = "action=downprocess&sign=$sign&ves=1"
+        reader = NetWorkUtil.post("https://wwa.lanzous.com/ajaxm.php", data.toByteArray(),
+                arrayOf("Referer", fnurl),
+                arrayOf("Cookie", "noads=1; pc_ad1=1"),
+                arrayOf("Host", "wwa.lanzous.com")
+        )!!.first.bufferedReader()
+        val magic = reader.readLine().split("\"")[9]
+        return "https://vip.d0.baidupan.com/file/$magic"
     }
 }
