244feb1e2dc63bf8eb369fda7ae18042d3cc978c
LovesAsuna <qq625924077@gmail.com>
2020-07-25 21:20:24 +0800

    [Change] 更改方法名

--- src/main/java/me/lovesasuna/bot/data/DependenceData.kt
+++ src/main/java/me/lovesasuna/bot/data/DependenceData.kt
@@ -35,7 +35,7 @@ interface DependenceData {
     }
 
     enum class Lanzous(override val data: String) : LanzousUrl {
-        CUSTOMCORE(DownloadUtil.getlanzousUrl("iHKpyey7bzg")),
+        CUSTOMCORE(DownloadUtil.Lanzou.getlanzousUrl("iHKpyey7bzg")),
     }
 
     enum class Maven(override val data: String) : MavenUrl {
