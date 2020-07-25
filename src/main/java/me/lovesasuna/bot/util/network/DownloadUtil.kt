b2d627b695baed9708e08f781806bb1dbc528f62
LovesAsuna <qq625924077@gmail.com>
2020-07-25 21:20:13 +0800

    [Add] 蓝奏云上传服务

--- src/main/java/me/lovesasuna/bot/util/network/DownloadUtil.kt
+++ src/main/java/me/lovesasuna/bot/util/network/DownloadUtil.kt
@@ -1,10 +1,12 @@
 package me.lovesasuna.bot.util.network
 
+import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
+import java.util.*
 import java.util.stream.Collectors
 
 /**
@@ -67,40 +69,116 @@ object DownloadUtil {
         conn.disconnect()
     }
 
-    /**
-     * @param id 蓝奏云地址后的字符串
-     * @param fileName 不包含路径的文件名
-     * @param savePath 不包含文件名的路径
-     * @param consumer 消费者函数，对下载时的字节长度进行处理
-     */
-    fun lanzousDownload(id: String, fileName: String, savePath: String, consumer: (Int) -> Unit = {}) {
-        download(getlanzousUrl(id), fileName, savePath, consumer, arrayOf("Accept-Language", "zh-CN,zh;q=0.9"))
-    }
+    object Lanzou {
+        /**
+         * @param id 蓝奏云地址后的字符串
+         * @param fileName 不包含路径的文件名
+         * @param savePath 不包含文件名的路径
+         * @param consumer 消费者函数，对下载时的字节长度进行处理
+         */
+        fun lanzousDownload(id: String, fileName: String, savePath: String, consumer: (Int) -> Unit = {}) {
+            DownloadUtil.download(getlanzousUrl(id), fileName, savePath, consumer, arrayOf("Accept-Language", "zh-CN,zh;q=0.9"))
+        }
 
-    /**
-     * @param id 蓝奏云地址后的字符串
-     */
-    fun getlanzousUrl(id: String): String {
-        val lanzousUrl = "https://wwa.lanzous.com/$id"
-        var reader = NetWorkUtil.get(lanzousUrl)!!.first.bufferedReader()
-        for (i in 0 until 45) reader.readLine()
-        val src = reader.readLine().also { reader.close() }
-        val fn = src.split("\"")[5]
-        val fnurl = "https://wwa.lanzous.com$fn"
-        reader = NetWorkUtil.get(fnurl)!!.first.bufferedReader()
-        val result = Regex("'\\w.*_c_c'").run {
-            this.find(reader.lines().filter { it.contains(this) }.collect(Collectors.toList()).first())!!.value
+        /**
+         * @param id 蓝奏云地址后的字符串
+         */
+        fun getlanzousUrl(id: String): String {
+            val lanzousUrl = "https://wwa.lanzous.com/$id"
+            var reader = NetWorkUtil.get(lanzousUrl)!!.first.bufferedReader()
+            for (i in 0 until 45) reader.readLine()
+            val src = reader.readLine().also { reader.close() }
+            val fn = src.split("\"")[5]
+            val fnurl = "https://wwa.lanzous.com$fn"
+            reader = NetWorkUtil.get(fnurl)!!.first.bufferedReader()
+            val result = Regex("'\\w.*_c_c'").run {
+                this.find(reader.lines().filter { it.contains(this) }.collect(Collectors.toList()).first())!!.value
+            }
+            val sign = result.split("'")[1]
+            val data = "action=downprocess&sign=$sign&ves=1"
+            reader = NetWorkUtil.post("https://wwa.lanzous.com/ajaxm.php", data.toByteArray(),
+                    arrayOf("Referer", fnurl),
+                    arrayOf("Cookie", "noads=1; pc_ad1=1"),
+                    arrayOf("Host", "wwa.lanzous.com")
+            )!!.first.bufferedReader()
+            val magic = reader.readLine().run {
+                split("\"")[9]
+            }
+            return "https://vip.d0.baidupan.com/file/$magic"
         }
-        val sign = result.split("'")[1]
-        val data = "action=downprocess&sign=$sign&ves=1"
-        reader = NetWorkUtil.post("https://wwa.lanzous.com/ajaxm.php", data.toByteArray(),
-                arrayOf("Referer", fnurl),
-                arrayOf("Cookie", "noads=1; pc_ad1=1"),
-                arrayOf("Host", "wwa.lanzous.com")
-        )!!.first.bufferedReader()
-        val magic = reader.readLine().run {
-            split("\"")[9]
+
+        fun uploadFile(file: File, cookie: String, user: String) {
+            val builder = StringBuilder()
+            val random = "test"
+            val contentType = "multipart/form-data; boundary=----WebKitFormBoundary$random"
+            val prefix = "------WebKitFormBoundary$random"
+            val inputStream = file.inputStream()
+            val size = file.length()
+            writeNode(builder, prefix, "task", "1")
+            writeNode(builder, prefix, "ve", "2")
+            writeNode(builder, prefix, "id", "WU_FILE_0")
+            writeNode(builder, prefix, "name", file.name)
+            writeNode(builder, prefix, "type", "application/x-zip-compressed")
+            writeNode(builder, prefix, "lastModifiedDate", "Thu Jan 1 2020 00:00:00 GMT+0800 (中国标准时间)")
+            writeNode(builder, prefix, "size", "$size")
+            writeNode(builder, prefix, "folder_id_bb_n", "-1")
+            builder.append(prefix)
+                    .append("\n")
+                    .append(disposition("upload_file\"; filename=\"${file.name}"))
+                    .append("\n")
+                    .append("Content-Type: application/x-zip-compressed")
+                    .append("\n")
+                    .append("\n")
+
+            val stream = ByteArrayOutputStream()
+            stream.write(builder.toString().toByteArray())
+            stream.write(inputStream.readBytes())
+            stream.write(("\n$prefix--").toByteArray())
+            val reader = NetWorkUtil.post("https://pc.woozooo.com/fileup.php",
+                    stream.toByteArray(),
+                    arrayOf("content-type", contentType),
+                    arrayOf("cookie", cookie),
+                    arrayOf("origin", "https://pc.woozooo.com"),
+                    arrayOf("referer", "https://pc.woozooo.com/mydisk.php?item=files&action=index&u=$user")
+            )!!.first.bufferedReader()
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                println(line)
+            }
         }
-        return "https://vip.d0.baidupan.com/file/$magic"
+
+
+        private fun disposition(name: String): String {
+            return "Content-Disposition: form-data; name=\"$name\""
+        }
+
+        private fun writeNode(builder: StringBuilder, prefix: String, name: String, content: String) {
+            builder.append(prefix)
+                    .append("\n")
+                    .append(disposition(name))
+                    .append("\n")
+                    .append("\n")
+                    .append(content)
+                    .append("\n")
+        }
+
+        private fun randomString(): String {
+            val maxNum = 36
+            var i: Int
+            var count = 0
+            val str = charArrayOf('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                    'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+                    'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+            val code = StringBuffer("")
+            val r = Random()
+            while (count < 16) {
+                i = Math.abs(r.nextInt(maxNum)) // 0到36的随机数
+                if (i >= 0 && i < str.size) { //如果i>=0且i<36
+                    code.append(str[i])
+                    count++
+                }
+            }
+            return code.toString()
+        }
     }
 }
