3056b7102bbe490d7e2ef8a2f176ea50c3103cb5
LovesAsuna <qq625924077@gmail.com>
2020-08-02 12:28:03 +0800

    [Add] 添加ConfigData

---
+++ src/main/java/me/lovesasuna/bot/data/ConfigData.kt
@@ -0,0 +1,8 @@
+package me.lovesasuna.bot.data
+
+data class ConfigData(var protocol: String = "ANDROID_PAD",
+                var account: Long = 0,
+                var admin: Long = 0,
+                var password: String = "",
+                var pictureSearchAPI: String = "",
+                var bilibiliCookie: String = "")
