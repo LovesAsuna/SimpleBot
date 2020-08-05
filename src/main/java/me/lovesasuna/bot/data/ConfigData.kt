1163c30380128fc40a6b12938f747a115cf14e24
LovesAsuna <qq625924077@gmail.com>
2020-08-05 20:04:57 +0800

    [Add] 添加蓝奏云Cookie

--- src/main/java/me/lovesasuna/bot/data/ConfigData.kt
+++ src/main/java/me/lovesasuna/bot/data/ConfigData.kt
@@ -1,8 +1,9 @@
 package me.lovesasuna.bot.data
 
 data class ConfigData(var protocol: String = "ANDROID_PAD",
-                var account: Long = 0,
-                var admin: Long = 0,
-                var password: String = "",
-                var pictureSearchAPI: String = "",
-                var bilibiliCookie: String = "")
+                      var account: Long = 0,
+                      var admin: Long = 0,
+                      var password: String = "",
+                      var pictureSearchAPI: String = "",
+                      var bilibiliCookie: String = "",
+                      var lanzouCookie: String = "")
