9c7d58bd9adc077335dd5111ccc4d597e95d5933
LovesAsuna <qq625924077@gmail.com>
2020-08-05 20:04:12 +0800

    [Refactor] 移动文件操作相关方法

--- src/main/java/me/lovesasuna/bot/util/BasicUtil.kt
+++ src/main/java/me/lovesasuna/bot/util/BasicUtil.kt
@@ -34,25 +34,6 @@ object BasicUtil {
         return message.replace(`var`, replace.toString())
     }
 
-    fun getFileMD5(file: File): String? {
-        return try {
-            val bytes = ByteArray(8192)
-            var len: Int
-            val inputStream = FileInputStream(file)
-            val messageDigest = MessageDigest.getInstance("MD5")
-            while (inputStream.read(bytes).also { len = it } != -1) {
-                messageDigest.update(bytes, 0, len)
-            }
-            inputStream.close()
-            val md5bytes = messageDigest.digest()
-            BigInteger(1, md5bytes).toString(16)
-        } catch (e: IOException) {
-            null
-        } catch (e: NoSuchAlgorithmException) {
-            null
-        }
-    }
-
     /**
      * @param command 任务
      * @param delay 延迟(单位:秒)
@@ -91,57 +72,6 @@ object BasicUtil {
 
     fun getLocation(fileName: String): File {
         return File("${getLocation(Main::class.java).path}${File.separator}$fileName")
-    }
-
-    fun toZip(srcDir: String, destFile: String) {
-        var zos: ZipOutputStream? = null
-        try {
-            zos = ZipOutputStream(FileOutputStream(destFile))
-            val sourceFile = File(srcDir)
-            compress(sourceFile, zos, sourceFile.name)
-        } catch (e: Exception) {
-            throw RuntimeException("zip error", e)
-        } finally {
-            if (zos != null) {
-                try {
-                    zos.close()
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-            }
-        }
-    }
-
-    /**
-     * 递归压缩方法
-     *
-     * @param sourceFile 源文件
-     * @param zos zip输出流
-     * @param name 压缩后的名称
-     * @throws Exception
-     */
-    private fun compress(sourceFile: File, zos: ZipOutputStream, name: String) {
-        val buf = ByteArray(2048)
-        if (sourceFile.isFile) {
-            zos.putNextEntry(ZipEntry(name))
-            var len: Int
-            val `in` = FileInputStream(sourceFile)
-            while (`in`.read(buf).also { len = it } != -1) {
-                zos.write(buf, 0, len)
-            }
-            zos.closeEntry()
-            `in`.close()
-        } else {
-            val listFiles = sourceFile.listFiles()
-            if (listFiles == null || listFiles.isEmpty()) {
-                zos.putNextEntry(ZipEntry("$name/"))
-                zos.closeEntry()
-            } else {
-                for (file in listFiles) {
-                    compress(file, zos, name + "/" + file.name)
-                }
-            }
-        }
     }
 
 }
