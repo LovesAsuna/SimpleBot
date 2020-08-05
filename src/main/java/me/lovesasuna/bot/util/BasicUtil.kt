4205370d5d61c445b99bc25a654db723ea561863
LovesAsuna <qq625924077@gmail.com>
2020-08-05 18:28:29 +0800

    [Add] 添加压缩工具

--- src/main/java/me/lovesasuna/bot/util/BasicUtil.kt
+++ src/main/java/me/lovesasuna/bot/util/BasicUtil.kt
@@ -4,22 +4,23 @@ import kotlinx.coroutines.*
 import me.lovesasuna.bot.Main
 import me.lovesasuna.bot.util.network.NetWorkUtil
 import me.lovesasuna.bot.util.plugin.PluginScheduler
-import java.io.File
-import java.io.FileInputStream
-import java.io.IOException
+import java.io.*
 import java.math.BigInteger
 import java.nio.file.Paths
 import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 import java.util.concurrent.TimeUnit
 import java.util.regex.Pattern
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+
 
 /**
  * @author LovesAsuna
  * @date 2020/4/19 14:05
  */
 object BasicUtil {
-    fun ExtraceInt(string: String, defaultValue: Int = 0): Int {
+    fun extractInt(string: String, defaultValue: Int = 0): Int {
         val pattern = Pattern.compile("\\d+")
         val buffer = StringBuffer()
         val matcher = pattern.matcher(string)
@@ -71,7 +72,11 @@ object BasicUtil {
     }
 
     fun debug(message: String): String {
-        val reader = NetWorkUtil.post("https://paste.ubuntu.com/", "poster=Bot&syntax=text&expiration=day&content=$message".toByteArray(Charsets.UTF_8), arrayOf("Content-Type", "application/x-www-form-urlencoded"), arrayOf("host", "paste.ubuntu.com"))!!.first.bufferedReader()
+        val reader = NetWorkUtil.post("https://paste.ubuntu.com/",
+                "poster=Bot&syntax=text&expiration=day&content=$message".toByteArray(Charsets.UTF_8),
+                arrayOf("Content-Type", "application/x-www-form-urlencoded"),
+                arrayOf("host", "paste.ubuntu.com")
+        )!!.first.bufferedReader()
         val builder = StringBuilder()
         reader.lines().skip(25).parallel().forEach {
             builder.append(it)
@@ -87,4 +92,56 @@ object BasicUtil {
     fun getLocation(fileName: String): File {
         return File("${getLocation(Main::class.java).path}${File.separator}$fileName")
     }
+
+    fun toZip(srcDir: String, destFile: String) {
+        var zos: ZipOutputStream? = null
+        try {
+            zos = ZipOutputStream(FileOutputStream(destFile))
+            val sourceFile = File(srcDir)
+            compress(sourceFile, zos, sourceFile.name)
+        } catch (e: Exception) {
+            throw RuntimeException("zip error", e)
+        } finally {
+            if (zos != null) {
+                try {
+                    zos.close()
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    /**
+     * 递归压缩方法
+     *
+     * @param sourceFile 源文件
+     * @param zos zip输出流
+     * @param name 压缩后的名称
+     * @throws Exception
+     */
+    private fun compress(sourceFile: File, zos: ZipOutputStream, name: String) {
+        val buf = ByteArray(2048)
+        if (sourceFile.isFile) {
+            zos.putNextEntry(ZipEntry(name))
+            var len: Int
+            val `in` = FileInputStream(sourceFile)
+            while (`in`.read(buf).also { len = it } != -1) {
+                zos.write(buf, 0, len)
+            }
+            zos.closeEntry()
+            `in`.close()
+        } else {
+            val listFiles = sourceFile.listFiles()
+            if (listFiles == null || listFiles.isEmpty()) {
+                zos.putNextEntry(ZipEntry("$name/"))
+                zos.closeEntry()
+            } else {
+                for (file in listFiles) {
+                    compress(file, zos, name + "/" + file.name)
+                }
+            }
+        }
+    }
+
 }
