2fa4e4b2cc1e626b67baadd8d4f9f675782b28ce
LovesAsuna <qq625924077@gmail.com>
2020-09-04 21:49:25 +0800

    [Feature] 退群操作

--- src/main/java/me/lovesasuna/bot/function/Admin.kt
+++ src/main/java/me/lovesasuna/bot/function/Admin.kt
@@ -1,5 +1,6 @@
 package me.lovesasuna.bot.function
 
+import kotlinx.coroutines.delay
 import me.lovesasuna.bot.file.Config
 import me.lovesasuna.bot.util.interfaces.FunctionListener
 import net.mamoe.mirai.message.GroupMessageEvent
@@ -7,20 +8,24 @@ import net.mamoe.mirai.message.MessageEvent
 import net.mamoe.mirai.message.data.Face
 import net.mamoe.mirai.message.data.Image
 import java.io.IOException
-import java.util.regex.Pattern
 
-class DeBug : FunctionListener {
-    private val pattern = Pattern.compile("/debug @")
+class Admin : FunctionListener {
+    private var confim = false
 
     @Throws(IOException::class)
     override suspend fun execute(event: MessageEvent, message: String, image: Image?, face: Face?): Boolean {
         event as GroupMessageEvent
         val senderID = event.sender.id
-        // val groupID = event.group.id
         if (senderID == Config.data.admin) {
-            if (message.startsWith("/debug")) {
-
+            if (message == "/quit" && !confim) {
+                event.reply("请在十秒内输入/quit confirm进行确认！")
+                confim = true
+                delay(10 * 1000)
+            } else if (message == "/quit confirm" && confim) {
+                event.reply("退群成功,感谢陪伴!")
+                event.group.quit()
             }
+            confim = false
         }
         return true
     }
