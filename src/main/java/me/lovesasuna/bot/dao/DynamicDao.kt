a1e309c1a8b742a32649db3d600b4e883fed476d
LovesAsuna <qq625924077@gmail.com>
2020-09-13 12:23:25 +0800

    [Feature] DynamicDao

--- src/main/java/me/lovesasuna/bot/dao/DynamicDao.kt
+++ src/main/java/me/lovesasuna/bot/dao/DynamicDao.kt
@@ -1,16 +1,24 @@
 package me.lovesasuna.bot.dao
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import me.lovesasuna.bot.entity.BotData
-import me.lovesasuna.bot.entity.DynamicEntity
+import me.lovesasuna.bot.entity.dynamic.DynamicEntity
 import org.hibernate.Session
 
 class DynamicDao(override val session: Session) : DefaultHibernateDao<DynamicEntity>(session) {
-    fun removeUP(id: Long) {
-        val listString  = queryField("select e.upSet from DynamicEntity as e", String::class.java)[0]
-        @Suppress("unchecked")
-        val list = jacksonObjectMapper().readValue(listString, List::class.java).toMutableList()
-        list.remove(1)
-        println(list)
+
+    fun updateDynamic(upID: Int, context: String) {
+        println("dao层更新前")
+        session.update(DynamicEntity(null, upID, context))
+        println("dao层更新完毕")
     }
+
+    fun getContext(upID: Int): String {
+        return queryField("select distinct e.context from DynamicEntity as e where upID = $upID", String::class.java).let {
+            if (it.isEmpty()) {
+                ""
+            } else {
+                it[0]
+            }
+        }
+    }
+
 }
