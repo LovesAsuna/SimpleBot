a35fdf893a30afeb252e6b33f62689cb1481e51e
LovesAsuna <qq625924077@gmail.com>
2020-09-13 12:20:33 +0800

    [Feature] 定义DynamicService实现类

--- src/main/java/me/lovesasuna/bot/service/impl/DynamicServiceImpl.kt
+++ src/main/java/me/lovesasuna/bot/service/impl/DynamicServiceImpl.kt
@@ -2,7 +2,6 @@ package me.lovesasuna.bot.service.impl
 
 import me.lovesasuna.bot.dao.DynamicDao
 import me.lovesasuna.bot.entity.BotData
-import me.lovesasuna.bot.entity.DynamicEntity
 import me.lovesasuna.bot.service.DynamicService
 import org.hibernate.SessionFactory
 
@@ -10,11 +9,23 @@ object DynamicServiceImpl : DynamicService {
 
     override val factory: SessionFactory = BotData.HibernateConfig.buildSessionFactory()
 
-    override fun save(entity: DynamicEntity) {
-
+    override fun update(upID: Int, context: String) {
+        val session = factory.openSession()
+        session.beginTransaction()
+        println("use前")
+        session.use { s ->
+            println("service update前")
+            DynamicDao(s).updateDynamic(upID, context)
+            println("service更新完毕")
+            session.transaction.commit()
+        }
     }
 
-    override fun update() {
-        TODO("Not yet implemented")
+    override fun getContext(upID: Int): String {
+        val session = factory.openSession()
+        session.use { s ->
+            return DynamicDao(s).getContext(upID)
+        }
     }
+
 }
