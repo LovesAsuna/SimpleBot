3b0dc5c1e8a06e2faeecfc6df307915490a344f7
LovesAsuna <qq625924077@gmail.com>
2020-09-13 12:24:33 +0800

    [Feature] HibernateDao

--- src/main/java/me/lovesasuna/bot/util/interfaces/dao/HibernateDao.kt
+++ src/main/java/me/lovesasuna/bot/util/interfaces/dao/HibernateDao.kt
@@ -5,7 +5,7 @@ interface HibernateDao<T> {
 
     fun <F>queryField(queryString: String, resultType: Class<F>, vararg prams: Any): List<F>
 
-    fun update(queryString: String, resultType: Class<T>, vararg prams: Any) : Int
+    fun update(queryString: String,  vararg prams: Any) : Int
 
     fun save(`object` : Any)
 }
