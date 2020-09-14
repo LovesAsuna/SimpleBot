d70abb8ef03ec4220a1460e6e6293fa2acdebd83
LovesAsuna <qq625924077@gmail.com>
2020-09-14 17:54:31 +0800

    [Refactor] 移除filter功能

--- src/main/java/me/lovesasuna/bot/util/annotations/Filter.kt
+++
@@ -1,5 +0,0 @@
-package me.lovesasuna.bot.util.annotations
-
-@Target(AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
-annotation class Filter
--- src/main/java/me/lovesasuna/bot/util/interfaces/processor/FilterProcessor.kt
+++
@@ -1,7 +0,0 @@
-package me.lovesasuna.bot.util.interfaces.processor
-
-interface FilterProcessor {
-    fun filter(groupID: Long): Boolean {
-        return false
-    }
-}
