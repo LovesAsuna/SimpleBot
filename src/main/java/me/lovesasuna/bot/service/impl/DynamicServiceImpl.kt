585b074dc78e60b3e6bde20b3b76c549bbeb550b
LovesAsuna <qq625924077@gmail.com>
2020-09-15 23:25:43 +0800

    [Refactor] 为每个service分配一个session

--- src/main/java/me/lovesasuna/bot/service/impl/DynamicServiceImpl.kt
+++ src/main/java/me/lovesasuna/bot/service/impl/DynamicServiceImpl.kt
@@ -1,31 +1,24 @@
 package me.lovesasuna.bot.service.impl
 
 import me.lovesasuna.bot.dao.DynamicDao
-import me.lovesasuna.bot.entity.BotData
+import me.lovesasuna.bot.data.BotData
+import me.lovesasuna.bot.entity.dynamic.DynamicEntity
 import me.lovesasuna.bot.service.DynamicService
+import org.hibernate.Session
 import org.hibernate.SessionFactory
 
 object DynamicServiceImpl : DynamicService {
 
-    override val factory: SessionFactory = BotData.HibernateConfig.buildSessionFactory()
+    override val session: Session = BotData.HibernateConfig.buildSessionFactory().openSession()
 
-    override fun update(upID: Int, context: String) {
-        val session = factory.openSession()
+    override fun update(upID: Long, context: String) {
         session.beginTransaction()
-        println("use前")
-        session.use { s ->
-            println("service update前")
-            DynamicDao(s).updateDynamic(upID, context)
-            println("service更新完毕")
-            session.transaction.commit()
-        }
+        val dao = DynamicDao(session)
+        val id = dao.getID(upID)
+        DynamicDao(session).updateDynamic(DynamicEntity(id, upID, context))
+        session.transaction.commit()
     }
 
-    override fun getContext(upID: Int): String {
-        val session = factory.openSession()
-        session.use { s ->
-            return DynamicDao(s).getContext(upID)
-        }
-    }
+    override fun getContext(upID: Long) = DynamicDao(session).getContext(upID)
 
 }
