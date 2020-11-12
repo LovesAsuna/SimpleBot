d55c563ef400bfc864170c6dc45793f54575fe71
LovesAsuna <qq625924077@gmail.com>
2020-11-12 18:46:22 +0800

    refactor(listener): 优雅的获取function

--- src/main/kotlin/me/lovesasuna/bot/listener/GroupMessageListener.kt
+++ src/main/kotlin/me/lovesasuna/bot/listener/GroupMessageListener.kt
@@ -1,21 +1,10 @@
 package me.lovesasuna.bot.listener
 
 import me.lovesasuna.bot.Main
-import me.lovesasuna.bot.controller.*
-import me.lovesasuna.bot.controller.bilibili.Danmu.Danmu
-import me.lovesasuna.bot.controller.bilibili.Bilibili
-import me.lovesasuna.bot.controller.bilibili.Dynamic
-import me.lovesasuna.bot.controller.game.McQuery
-import me.lovesasuna.bot.controller.game.RainbowSix
-import me.lovesasuna.bot.controller.misc.Baike
-import me.lovesasuna.bot.controller.misc.Hitokoto
-import me.lovesasuna.bot.controller.photo.Photo
-import me.lovesasuna.bot.controller.photo.PictureSearch
-import me.lovesasuna.bot.controller.photo.PixivGetter
-import me.lovesasuna.bot.controller.qqfun.*
-import me.lovesasuna.bot.file.Config
-import me.lovesasuna.bot.util.interfaces.EventListener
 import me.lovesasuna.bot.controller.FunctionListener
+import me.lovesasuna.bot.file.Config
+import me.lovesasuna.bot.util.annotations.processors.ProcessorHandler
+import me.lovesasuna.bot.util.interfaces.EventListener
 import me.lovesasuna.bot.util.plugin.Logger
 import net.mamoe.mirai.event.subscribeAlways
 import net.mamoe.mirai.message.GroupMessageEvent
@@ -26,15 +15,7 @@ object GroupMessageListener : EventListener {
     private val listeners = ArrayList<FunctionListener>()
 
     init {
-        val listenersClass = arrayOf<Class<*>>(
-            KeyWord::class.java, McQuery::class.java,
-            Bilibili::class.java, Hitokoto::class.java,
-            Admin::class.java, RainbowSix::class.java,
-            RepeatDetect::class.java, PictureSearch::class.java,
-            PixivGetter::class.java, Danmu::class.java, Photo::class.java,
-            Dynamic::class.java, Baike::class.java, Nbnhhsh::class.java,
-            Notice::class.java, Card::class.java
-        )
+        val listenersClass = ProcessorHandler.getClasses(FunctionListener::class.java.typeName.substringBeforeLast("."))
         listenersClass.filter {
             !Config.data.DisableFunction.contains(it.simpleName)
         }.forEach {
