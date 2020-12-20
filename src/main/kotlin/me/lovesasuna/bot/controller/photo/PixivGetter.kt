9a399067886e5ddca124c567b663b103de5061ea
LovesAsuna <qq625924077@gmail.com>
2020-12-20 10:18:50 +0800

    fix(function-pixivgetter): 扫描api无了(悲)

--- src/main/kotlin/me/lovesasuna/bot/controller/photo/PixivGetter.kt
+++ src/main/kotlin/me/lovesasuna/bot/controller/photo/PixivGetter.kt
@@ -10,36 +10,17 @@ import me.lovesasuna.lanzou.util.NetWorkUtil
 import java.io.InputStream
 
 class PixivGetter : FunctionListener {
-
-
     @ExperimentalCoroutinesApi
     override suspend fun execute(box: MessageBox): Boolean {
         when {
             box.text().startsWith("/pixiv work ") -> {
                 val ID = BasicUtil.extractInt(box.text().split(" ")[2])
-                var reader = NetWorkUtil["https://api.imjad.cn/pixiv/v1/?type=illust&id=$ID"]!!.second.bufferedReader()
-                var root = BotData.objectMapper.readTree(reader.readLine())
-                val status = root["status"].asText()
-                if (BotData.debug) box.reply("R级检测响应: $status")
-                var count = 1
-                if (status == "failure") {
-                    box.reply("查询图片信息失败，跳过R级检测...")
-                } else {
-                    val tags = root["response"][0]["tags"].toString()
-                    count = root["response"][0]["page_count"].asInt()
-                    if (BotData.debug) box.reply(tags)
-                    if (tags.contains(Regex("R-[1-9]+"))) {
-                        box.reply("图片含有R18内容,禁止显示！")
-                        return false
-                    }
-                }
-
                 box.reply("获取中,请稍后..")
-                reader = NetWorkUtil.post(
+                val reader = NetWorkUtil.post(
                     "https://api.pixiv.cat/v1/generate", "p=$ID".toByteArray(),
                     arrayOf("content-type", "application/x-www-form-urlencoded; charset=UTF-8")
                 )!!.second.bufferedReader()
-                root = BotData.objectMapper.readTree(reader.readLine())
+                val root = BotData.objectMapper.readTree(reader.readLine())
                 val list = root.get("original_url") ?: root.get("original_urls")
                 if (list == null) {
                     box.reply("该作品不存在或已被删除!")
@@ -59,7 +40,7 @@ class PixivGetter : FunctionListener {
                         box.reply("图片获取失败,大概率是服务器宽带问题或图片过大，请捐赠支持作者")
                     }
                 } else {
-                    box.reply("该作品共有${count}张图片")
+                    box.reply("该作品共有${size}张图片")
                     repeat(size) {
                         originInputStream =
                             NetWorkUtil["https://api.kuku.me/pixiv/picbyurl?url=${list[it].asText()}"]!!.second
