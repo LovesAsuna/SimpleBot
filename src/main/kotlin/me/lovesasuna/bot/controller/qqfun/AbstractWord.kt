a8fa73e31de1d4adb5cf2e8270e569bfc7f5f499
LovesAsuna <qq625924077@gmail.com>
2021-01-03 10:15:22 +0800

    fix(funciton): 修复抽象话无法使用

--- src/main/kotlin/me/lovesasuna/bot/controller/qqfun/AbstractWord.kt
+++ src/main/kotlin/me/lovesasuna/bot/controller/qqfun/AbstractWord.kt
@@ -1,32 +1,28 @@
 package me.lovesasuna.bot.controller.qqfun
 
+import me.lovesasuna.bot.Main
 import me.lovesasuna.bot.controller.FunctionListener
 import me.lovesasuna.bot.data.MessageBox
-import me.lovesasuna.lanzou.util.NetWorkUtil
-import java.io.IOException
 import javax.script.ScriptEngineManager
-import javax.script.ScriptException
 
 
 /**
  * @author LovesAsuna
  **/
 class AbstractWord : FunctionListener {
+    val se = ScriptEngineManager().getEngineByName("JavaScript")
+
     override suspend fun execute(box: MessageBox): Boolean {
         val message = box.text()
         if (!message.startsWith("/抽象话 ")) {
             return false
         }
-        val se = ScriptEngineManager().getEngineByName("JavaScript")
         try {
-            val str = NetWorkUtil["https://share.kuku.me/189/kuku/chouxianghua.js"]!!.second.bufferedReader().readText()
+            val str =  Main.javaClass.classLoader.getResourceAsStream("chouxianghua.js").bufferedReader().readText()
             se.eval(str)
             val o = se.eval("chouxiang(\"" + message.split(" ")[1] + "\")")
-            o.toString()
-        } catch (e: ScriptException) {
-            e.printStackTrace()
-            box.reply("生成失败，请重试！！")
-        } catch (e: IOException) {
+            box.reply(o.toString())
+        } catch (e: Exception) {
             e.printStackTrace()
             box.reply("生成失败，请重试！！")
         }
