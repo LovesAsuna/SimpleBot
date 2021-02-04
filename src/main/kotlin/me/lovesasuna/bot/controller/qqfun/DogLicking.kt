040d329e5c7751dd8d769c1c4b2d075278dcdb16
LovesAsuna <qq625924077@gmail.com>
2021-02-04 13:29:28 +0800

    fix(function): 切换舔狗日记接口

--- src/main/kotlin/me/lovesasuna/bot/controller/qqfun/DogLicking.kt
+++ src/main/kotlin/me/lovesasuna/bot/controller/qqfun/DogLicking.kt
@@ -1,10 +1,10 @@
 package me.lovesasuna.bot.controller.qqfun
 
 import me.lovesasuna.bot.controller.FunctionListener
-import me.lovesasuna.bot.data.BotData
 import me.lovesasuna.bot.data.MessageBox
 import me.lovesasuna.lanzou.util.NetWorkUtil
-
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 /**
  * @author LovesAsuna
@@ -14,14 +14,8 @@ class DogLicking : FunctionListener {
         if (box.text() != "/舔狗日记") {
             return false
         }
-        val node = BotData.objectMapper.readTree(NetWorkUtil["http://api.yyhy.me/tg.php?type=api"]!!.second)
-        box.reply(
-            if (node["code"].asInt() == 1) {
-                node["date"].asText() + "\n" + node["content"].asText()
-            } else {
-                "获取失败！！"
-            }
-        )
+        val reader = BufferedReader(InputStreamReader(NetWorkUtil["https://v1.alapi.cn/api/dog?format=text"]!!.second))
+        box.reply(reader.readLine())
         return true
     }
 }
