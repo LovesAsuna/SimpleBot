c98a5a8e05aa3383bcf89c5c489ec36320cea054
LovesAsuna <qq625924077@gmail.com>
2021-02-06 16:20:38 +0800

    fix(function): 修复偶尔的NPE

--- src/main/kotlin/me/lovesasuna/bot/util/pictureSearch/Saucenao.kt
+++ src/main/kotlin/me/lovesasuna/bot/util/pictureSearch/Saucenao.kt
@@ -1,12 +1,13 @@
 package me.lovesasuna.bot.util.pictureSearch
 
+import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import me.lovesasuna.bot.Config
 import me.lovesasuna.lanzou.util.NetWorkUtil
 
 object Saucenao : PictureSearchSource {
     private val api =
-        //todo api
-        "https://saucenao.com/search.php?db=999&output_type=2&testmode=1&api_key=${"api"}&numres=16&url="
+        "https://saucenao.com/search.php?db=999&output_type=2&testmode=1&api_key=${Config.SauceNaoAPI}&numres=16&url="
     private val mapper = ObjectMapper()
 
     override fun search(url: String): List<Result> {
@@ -19,8 +20,11 @@ object Saucenao : PictureSearchSource {
                 val similarity = result["header"]["similarity"].asInt()
                 if (similarity < 57.5) continue
                 val extUrlsList = ArrayList<String>()
-                repeat(result["data"]["ext_urls"].size()) {
-                    extUrlsList.add(result["data"]["ext_urls"][it].asText())
+                val exeUrls: JsonNode? = result["data"]["ext_urls"]
+                if (exeUrls != null) {
+                    repeat(exeUrls.size()) {
+                        extUrlsList.add(result["data"]["ext_urls"][it].asText())
+                    }
                 }
                 if (!extUrlsList.parallelStream().anyMatch { it.contains("pixiv") }) continue
                 val thumbnail = result["header"]["thumbnail"].asText()
