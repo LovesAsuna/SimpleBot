3cc958abd3064c97fa79163548560ed014abdc87
LovesAsuna <qq625924077@gmail.com>
2021-02-14 22:11:51 +0800

    refactor(util): 采用OkHttpUtil

--- src/main/kotlin/me/lovesasuna/bot/controller/photo/source/Pixiv.kt
+++ src/main/kotlin/me/lovesasuna/bot/controller/photo/source/Pixiv.kt
@@ -3,7 +3,7 @@ package me.lovesasuna.bot.controller.photo.source
 import me.lovesasuna.bot.data.BotData.objectMapper
 import me.lovesasuna.bot.data.pushError
 import me.lovesasuna.bot.file.Config
-import me.lovesasuna.lanzou.util.NetWorkUtil
+import me.lovesasuna.bot.util.network.OkHttpUtil
 import java.io.IOException
 
 /**
@@ -12,10 +12,9 @@ import java.io.IOException
 class Pixiv : PhotoSource {
     override fun fetchData(): String? {
         val source = "https://api.lolicon.app/setu/?apikey=${Config.data.API["LoliconAPI"]?.random()}"
-        val result = NetWorkUtil[source]
+        val result =  OkHttpUtil.getIs(OkHttpUtil[source])
         return try {
-            val inputStream = result!!.second
-            val root = objectMapper.readTree(inputStream)
+            val root = objectMapper.readTree(result)
             val quota = root["quota"].asText()
             val url = root["data"][0]?.let { it["url"].asText() } ?: return "|0"
             return "$url|$quota"
