1d7f54dc811e3d9c73d145452e6f74a2e9476b36
LovesAsuna <qq625924077@gmail.com>
2021-02-27 23:50:01 +0800

    refactor(function): 更简洁的表达

--- src/main/kotlin/me/lovesasuna/bot/util/protocol/SRVConvertUtil.kt
+++ src/main/kotlin/me/lovesasuna/bot/util/protocol/SRVConvertUtil.kt
@@ -1,14 +1,14 @@
 package me.lovesasuna.bot.util.protocol
 
-import java.nio.charset.Charset
-
 object SRVConvertUtil {
     private val runtime = Runtime.getRuntime()
 
     fun convert(host: String): String {
         val process = runtime.exec("cmd /c nslookup -qt=SRV _minecraft._tcp.$host")
-        val reader = process.inputStream.bufferedReader(Charset.forName("GBK"))
-        (0..5).forEach { _ -> reader.readLine() }
+        val reader = process.inputStream.bufferedReader()
+        repeat(6) {
+            reader.readLine()
+        }
         val port = reader.readLine().split("=")[1].trim()
         val ip = reader.readLine().split("=")[1].trim()
         return "$ip:$port"
