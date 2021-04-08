4837d71ae0401f7fe28b6a7bcb3fa36c4bb50dc5
LovesAsuna <qq625924077@gmail.com>
2021-04-08 18:52:56 +0800

    fix(function):
    重新添加R18检测，并且删除加速链接获取

--- src/main/kotlin/me/lovesasuna/bot/controller/photo/PixivGetter.kt
+++ src/main/kotlin/me/lovesasuna/bot/controller/photo/PixivGetter.kt
@@ -2,57 +2,56 @@ package me.lovesasuna.bot.controller.photo
 
 import me.lovesasuna.bot.Main
 import me.lovesasuna.bot.data.BotData
-import me.lovesasuna.lanzou.util.OkHttpUtil
 import me.lovesasuna.bot.util.registerDefaultPermission
+import me.lovesasuna.lanzou.util.OkHttpUtil
 import net.mamoe.mirai.console.command.CommandSender
-import net.mamoe.mirai.console.command.SimpleCommand
+import net.mamoe.mirai.console.command.CompositeCommand
 import net.mamoe.mirai.console.command.getGroupOrNull
 import net.mamoe.mirai.utils.ExternalResource.Companion.uploadAsImage
 import java.io.InputStream
 
-object PixivGetter : SimpleCommand(
+object PixivGetter : CompositeCommand(
     owner = Main,
     primaryName = "pixiv",
     description = "反代理P站图片",
     parentPermission = registerDefaultPermission()
 ) {
-    @Handler
-    suspend fun CommandSender.handle(command: String, ID: Int) {
-        if (command != "work") {
-            return
-        }
-        sendMessage("获取中,请稍后..")
-        val reader = OkHttpUtil.getIs(
-            OkHttpUtil.post(
-                "https://api.pixiv.cat/v1/generate", mapOf(
-                    "p" to "$ID"
-                )
-            )
-        ).bufferedReader()
-        val root = BotData.objectMapper.readTree(reader.readLine())
-        val list = root.get("original_url") ?: root.get("original_urls")
-        if (list == null) {
+    @SubCommand
+    suspend fun CommandSender.work(ID: Int) {
+        var count = 1
+        val root = OkHttpUtil.getJson("https://api.obfs.dev/api/pixiv/illust?id=$ID")
+        if (root["error"] != null) {
             sendMessage("该作品不存在或已被删除!")
             return
         }
-        val size = list.size()
+        count = root["illust"]["page_count"].asInt()
+        val tags = root["illust"]["tags"].toString()
+        if (tags.contains(Regex("R-[1-9]+"))) {
+            sendMessage("图片含有R18内容,禁止显示！")
+            return
+        }
+        sendMessage("获取中,请稍后..")
         var originInputStream: InputStream?
-        if (size == 0) {
+        if (count == 1) {
             if (BotData.debug) sendMessage("尝试复制IO流")
             Main.scheduler.withTimeOut(suspend {
                 originInputStream =
-                    OkHttpUtil.getIs(OkHttpUtil["https://api.kuku.me/pixiv/picbyurl?url=${list.asText()}"])
+                    OkHttpUtil.getIs(OkHttpUtil["https://pixiv.cat/$ID.jpg"])
                 sendMessage(originInputStream!!.uploadAsImage(getGroupOrNull()!!))
                 sendMessage("获取完成!")
             }, 60 * 1000) {
                 sendMessage("图片获取失败,大概率是服务器宽带问题或图片过大，请捐赠支持作者")
             }
         } else {
-            sendMessage("该作品共有${size}张图片")
-            repeat(size) {
-                originInputStream =
-                    OkHttpUtil.getIs(OkHttpUtil["https://api.kuku.me/pixiv/picbyurl?url=${list[it].asText()}"])
-                sendMessage(originInputStream!!.uploadAsImage(getGroupOrNull()!!))
+            sendMessage("该作品共有${count}张图片")
+            repeat(count) {
+                Main.scheduler.withTimeOut(suspend {
+                    originInputStream =
+                        OkHttpUtil.getIs(OkHttpUtil["https://pixiv.cat/$ID-${it + 1}.jpg"])
+                    sendMessage(originInputStream!!.uploadAsImage(getGroupOrNull()!!))
+                }, 60 * 1000) {
+                    sendMessage("图片获取失败,大概率是服务器宽带问题或图片过大，请捐赠支持作者")
+                }
             }
             sendMessage("获取完成!")
         }
