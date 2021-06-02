3eb31e94ce216b240b98e59f80e5301c6607731e
LovesAsuna <qq625924077@gmail.com>
2021-06-02 18:21:50 +0800

    fix(orm): 修复Hierbante无法在java11使用的问题

--- src/main/kotlin/me/lovesasuna/bot/data/BotData.kt
+++ src/main/kotlin/me/lovesasuna/bot/data/BotData.kt
@@ -2,12 +2,10 @@ package me.lovesasuna.bot.data
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import me.lovesasuna.bot.Main
 import me.lovesasuna.bot.util.ClassUtil
 import org.hibernate.cfg.Configuration
-import java.util.*
 
 /**
  * @author LovesAsuna
@@ -22,6 +20,7 @@ object BotData {
         ClassUtil.getClasses("me.lovesasuna.bot.entity", Main::class.java.classLoader).forEach { c ->
             it.addAnnotatedClass(c)
         }
+        Thread.currentThread().contextClassLoader = this.javaClass.classLoader
         it.configure()
     }
 }
