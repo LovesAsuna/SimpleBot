d25703c80de2f4383603471bd76b7fa0915786b6
LovesAsuna <qq625924077@gmail.com>
2021-08-20 16:07:35 +0800

    feat(data): smarter automatic configuration

--- src/main/kotlin/me/lovesasuna/bot/data/BotData.kt
+++ src/main/kotlin/me/lovesasuna/bot/data/BotData.kt
@@ -14,21 +14,34 @@ object BotData {
     var objectMapper: ObjectMapper =
         jacksonObjectMapper().also { it.propertyNamingStrategy = PropertyNamingStrategies.LOWER_CASE }
 
-    val configBlock: (Configuration, String) -> Unit = { config, string ->
-        ClassUtil.getClasses("me.lovesasuna.bot.entity", Main::class.java.classLoader).forEach { c ->
-            config.addAnnotatedClass(c)
+    private val configBlock: (config: Configuration, configFile: String, packages: Array<String>) -> Unit =
+        { config, string, packages ->
+            packages.forEach {
+                ClassUtil.getClasses(it, Main::class.java.classLoader).forEach { c ->
+                    config.addAnnotatedClass(c)
+                }
+            }
+            val classLoader = Thread.currentThread().contextClassLoader
+            Thread.currentThread().contextClassLoader = this.javaClass.classLoader
+            config.configure(string)
+            Thread.currentThread().contextClassLoader = classLoader
         }
-        val classLoader = Thread.currentThread().contextClassLoader
-        Thread.currentThread().contextClassLoader = this.javaClass.classLoader
-        config.configure(string)
-        Thread.currentThread().contextClassLoader = classLoader
-    }
 
     val functionConfig: Configuration = Configuration().also {
-      configBlock.invoke(it, "function.xml")
+        configBlock.invoke(
+            it, "function.xml", arrayOf(
+                "me.lovesasuna.bot.entity.dynamic",
+                "me.lovesasuna.bot.entity.fun",
+                "me.lovesasuna.bot.entity.game"
+            )
+        )
     }
 
     val recordConfig: Configuration = Configuration().also {
-        configBlock.invoke(it, "record.xml")
+        configBlock.invoke(
+            it, "record.xml", arrayOf(
+                "me.lovesasuna.bot.entity.message"
+            )
+        )
     }
 }
