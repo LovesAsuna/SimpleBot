76f4773861c93da664b3db63ad5eea597f8ca58f
LovesAsuna <qq625924077@gmail.com>
2021-08-21 21:01:53 +0800

    feat(function): repeat support emoji

--- src/main/kotlin/me/lovesasuna/bot/controller/qqfun/RepeatDetect.kt
+++ src/main/kotlin/me/lovesasuna/bot/controller/qqfun/RepeatDetect.kt
@@ -30,6 +30,30 @@ class RepeatDetect : FunctionListener {
     private val gifDecoder = GifDecoder()
     private val gifEncoder = AnimatedGifEncoder()
     private val random = Random()
+    private val textPattern = "[\\u4e00-\\u9fa5_a-zA-Z0-9]|" +
+            "[\\uD83C\\uDF00-\\uD83D\\uDDFF]|" +
+            "[\\uD83E\\uDD00-\\uD83E\\uDDFF]|" +
+            "[\\uD83D\\uDE00-\\uD83D\\uDE4F]|" +
+            "[\\uD83D\\uDE80-\\uD83D\\uDEFF]|" +
+            "[\\u2600-\\u26FF]\\uFE0F?|" +
+            "[\\u2700-\\u27BF]\\uFE0F?|" +
+            "\\u24C2\\uFE0F?|" +
+            "[\\uD83C\\uDDE6-\\uD83C\\uDDFF]{1,2}|" +
+            "[\\uD83C\\uDD70\\uD83C\\uDD71\\uD83C\\uDD7E\\uD83C\\uDD7F\\uD83C\\uDD8E\\uD83C\\uDD91-\\uD83C\\uDD9A]\\uFE0F?|" +
+            "[\\u0023\\u002A\\u0030-\\u0039]\\uFE0F?\\u20E3|" +
+            "[\\u2194-\\u2199\\u21A9-\\u21AA]\\uFE0F?|" +
+            "[\\u2B05-\\u2B07\\u2B1B\\u2B1C\\u2B50\\u2B55]\\uFE0F?|" +
+            "[\\u2934\\u2935]\\uFE0F?|" +
+            "[\\u3030\\u303D]\\uFE0F?|" +
+            "[\\u3297\\u3299]\\uFE0F?|" +
+            "[\\uD83C\\uDE01\\uD83C\\uDE02\\uD83C\\uDE1A\\uD83C\\uDE2F\\uD83C\\uDE32-\\uD83C\\uDE3A\\uD83C\\uDE50\\uD83C\\uDE51]\\uFE0F?|" +
+            "[\\u203C\\u2049]\\uFE0F?|" +
+            "[\\u25AA\\u25AB\\u25B6\\u25C0\\u25FB-\\u25FE]\\uFE0F?|" +
+            "[\\u00A9\\u00AE]\\uFE0F?|" +
+            "[\\u2122\\u2139]\\uFE0F?|" +
+            "\\uD83C\\uDC04\\uFE0F?|" +
+            "\\uD83C\\uDCCF\\uFE0F?|" +
+            "[\\u231A\\u231B\\u2328\\u23CF\\u23E9-\\u23F3\\u23F8-\\u23FA]\\uFE0F?"
 
     override suspend fun execute(box: MessageBox): Boolean {
         val groupID = box.group!!.id
@@ -89,13 +113,9 @@ class RepeatDetect : FunctionListener {
     private fun randomText(text: PlainText): MessageChain {
         return buildMessageChain {
             if (random.nextBoolean()) {
-                mutableListOf(text.content.split("")).apply {
-                    this.shuffle()
-                    joinToString("")
-                }
-                ArrayList<Char>().apply {
-                    text.content.forEach { c ->
-                        this.add(c)
+                ArrayList<String>().apply {
+                    Regex(textPattern).findAll(text.content).forEach {
+                        this.add(it.value)
                     }
                     this.shuffle()
                     val builder = StringBuilder()
@@ -110,7 +130,7 @@ class RepeatDetect : FunctionListener {
         }
     }
 
-    private suspend fun randomImage(box : MessageBox, image: Image): MessageChain {
+    private suspend fun randomImage(box: MessageBox, image: Image): MessageChain {
         val url = image.queryUrl()
         val cloneInputStream = OkHttpUtil.inputStreamClone(OkHttpUtil.getIs(OkHttpUtil[url]))!!
         return buildMessageChain {
