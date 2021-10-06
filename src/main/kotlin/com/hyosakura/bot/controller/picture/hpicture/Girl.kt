744edaa0059c59006946a82333153b8ea77d3518
LovesAsuna <qq625924077@gmail.com>
2021-10-06 11:18:55 +0800

    :sparkles: feat(picture source): new picture source

---
+++ src/main/kotlin/com/hyosakura/bot/controller/picture/hpicture/Girl.kt
@@ -0,0 +1,12 @@
+package com.hyosakura.bot.controller.picture.hpicture
+
+import kotlin.random.Random
+
+/**
+ * @author LovesAsuna
+ **/
+class Girl : SinglePictureSource {
+    override fun fetchData(): String {
+        return "https://api.nmb.show/xiaojiejie${Random.nextInt(1, 3)}.php"
+    }
+}
