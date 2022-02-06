9cbfd8602aa02af7a4a0ee2e0c3db712b698f77c
LovesAsuna <qq625924077@gmail.com>
2022-02-06 17:23:35 +0800

    :zap: perf(interface): return flow

--- src/main/kotlin/com/hyosakura/bot/controller/picture/hpicture/MultiPictureSource.kt
+++ src/main/kotlin/com/hyosakura/bot/controller/picture/hpicture/MultiPictureSource.kt
@@ -1,10 +1,13 @@
 package com.hyosakura.bot.controller.picture.hpicture
 
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
+
 /**
  * @author LovesAsuna
  **/
 interface MultiPictureSource : SinglePictureSource {
-    suspend fun fetchData(num: Int): List<String>
+    suspend fun fetchData(num: Int): Flow<String>
 
     override suspend fun fetchData(): String? = fetchData(1).firstOrNull()
 }
