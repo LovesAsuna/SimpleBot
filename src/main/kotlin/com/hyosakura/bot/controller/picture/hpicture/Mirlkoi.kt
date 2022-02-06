cce9141e2b4cae3f5665d7ff9cacf487e618a8e0
LovesAsuna <qq625924077@gmail.com>
2022-02-06 19:17:21 +0800

    :recycle: perf(performance): emit image url directly

--- src/main/kotlin/com/hyosakura/bot/controller/picture/hpicture/Mirlkoi.kt
+++ src/main/kotlin/com/hyosakura/bot/controller/picture/hpicture/Mirlkoi.kt
@@ -1,6 +1,5 @@
 package com.hyosakura.bot.controller.picture.hpicture
 
-import com.hyosakura.bot.util.network.Request
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
@@ -9,12 +8,10 @@ import kotlinx.coroutines.flow.flow
  */
 class Mirlkoi : MultiPictureSource {
     override suspend fun fetchData(num: Int): Flow<String> {
-        val source = "http://mobile.fgimaxl2.vipnps.vip/API/GHS/3233161559.php?type=json"
+        val source = "http://mobile.fgimaxl2.vipnps.vip/API/GHS/3233161559.php"
         return flow {
             repeat(num) {
-                val root = Request.getJson(source)
-                val url = root["pic"].asText()
-                emit(url)
+                emit(source)
             }
         }
     }
