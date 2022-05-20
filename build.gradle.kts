d8795b51eb636ca743fbd1b972db31d9b970d3ce
LovesAsuna <qq625924077@gmail.com>
2022-05-20 21:35:46 +0800

    :arrow_up: build(gradle): upgrade dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 
 val workDir = "E:\\work\\Mirai-Console\\plugins"
 val kotlinVersion = "1.6.10"
-val miraiVersion = "2.10.2"
+val miraiVersion = "2.11.0"
 
 // mirai
 dependencies {
