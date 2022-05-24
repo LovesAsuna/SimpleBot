a34eedbcafa9a336e88d1de391cb3b329d0a8c6c
LovesAsuna <qq625924077@gmail.com>
2022-05-24 23:02:00 +0800

    :bug: fix(name): the name of member may be null

--- src/main/kotlin/com/hyosakura/bot/listener/MemberLeaveListener.kt
+++ src/main/kotlin/com/hyosakura/bot/listener/MemberLeaveListener.kt
@@ -6,7 +6,7 @@ import net.mamoe.mirai.event.events.MemberLeaveEvent
 object MemberLeaveListener : EventListener {
     override fun onAction() {
         Main.eventChannel.subscribeAlways(MemberLeaveEvent::class) {
-            group.sendMessage("刚刚，${member.nameCard}(${member.id})离开了我们！！")
+            group.sendMessage("刚刚，${member.nick}(${member.id})离开了我们！！")
         }
     }
 
