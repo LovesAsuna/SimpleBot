089c1a940e4ab5f8ed4ad6113e25d97b209c830b
LovesAsuna <qq625924077@gmail.com>
2022-10-01 19:41:41 +0800

    :sparkles: feat(feature): add pixiv proxy feature

--- simple_bot/src/plugin/internal/picture/pixiv_proxy.rs
+++ simple_bot/src/plugin/internal/picture/pixiv_proxy.rs
@@ -51,49 +51,49 @@ async fn get(event: &MessageEvent, id: Option<String>) -> anyhow::Result<bool> {
         return Ok(false);
     }
     let id = id.unwrap();
-    // let api = format!("https://api.obfs.dev/api/pixiv/illust?id={id}");
-    // let resp = tokio::time::timeout(Duration::from_secs(15), reqwest::get(api)).await;
-    // if resp.is_err() {
-    //     event.send_message_to_source("Error: 获取图像信息超时".parse_message_chain()).await.unwrap();
-    //     return Ok(false);
-    // }
-    // let text = resp.unwrap()?.text().await?;
-    // let root: serde_json::Value = serde_json::from_str(&text)?;
-    // if let serde_json::value::Value::Object(err) = &root["error"] {
-    //     let mut text: &str = "";
-    //     let message = String::from("message");
-    //     let user_message = String::from("user_message");
-    //     text = err[&message].as_str().unwrap_or("");
-    //     if text.is_empty() {
-    //         text = err[&user_message].as_str().unwrap_or("");
-    //     }
-    //     event.send_message_to_source(format!("Error: {text}").parse_message_chain()).await.unwrap();
-    //     return Ok(false);
-    // }
-    // let illustration = &root["illust"];
-    // let title = illustration["title"].as_str().unwrap_or("");
-    // let user_name = illustration["user"]["name"].as_str().unwrap_or("");
-    // let account = illustration["user"]["account"].as_str().unwrap_or("");
-    // let mut time = DateTime::parse_from_rfc3339(illustration["create_date"].as_str().unwrap())?;
-    // let format = format!(
-    //     "%Y年%m月%d日{}%H点%M分",
-    //     if time.hour() <= 12 {
-    //         "上午"
-    //     } else if time.hour() <= 18 {
-    //         time.sub_assign(chrono::Duration::hours(12));
-    //         "下午"
-    //     } else {
-    //         time.sub_assign(chrono::Duration::hours(12));
-    //         "晚上"
-    //     }
-    // );
-    // let time_text = time.format(&format).to_string();
-    // let tags = flatten_tags(&illustration["tags"]);
-    // let r18 = validate(&tags);
-    // let count = illustration["page_count"].as_u64().unwrap_or(0);
-    // let view = illustration["total_view"].as_u64().unwrap_or(0);
-    // let mark = illustration["total_bookmarks"].as_u64().unwrap_or(0);
-    // build message
+    let api = format!("https://api.obfs.dev/api/pixiv/illust?id={id}");
+    let resp = tokio::time::timeout(Duration::from_secs(15), reqwest::get(api)).await;
+    if resp.is_err() {
+        event.send_message_to_source("Error: 获取图像信息超时".parse_message_chain()).await.unwrap();
+        return Ok(false);
+    }
+    let text = resp.unwrap()?.text().await?;
+    let root: serde_json::Value = serde_json::from_str(&text)?;
+    if let serde_json::value::Value::Object(err) = &root["error"] {
+        let mut text: &str = "";
+        let message = String::from("message");
+        let user_message = String::from("user_message");
+        text = err[&message].as_str().unwrap_or("");
+        if text.is_empty() {
+            text = err[&user_message].as_str().unwrap_or("");
+        }
+        event.send_message_to_source(format!("Error: {text}").parse_message_chain()).await.unwrap();
+        return Ok(false);
+    }
+    let illustration = &root["illust"];
+    let title = illustration["title"].as_str().unwrap_or("");
+    let user_name = illustration["user"]["name"].as_str().unwrap_or("");
+    let account = illustration["user"]["account"].as_str().unwrap_or("");
+    let mut time = DateTime::parse_from_rfc3339(illustration["create_date"].as_str().unwrap())?;
+    let format = format!(
+        "%Y年%m月%d日{}%H点%M分",
+        if time.hour() <= 12 {
+            "上午"
+        } else if time.hour() <= 18 {
+            time.sub_assign(chrono::Duration::hours(12));
+            "下午"
+        } else {
+            time.sub_assign(chrono::Duration::hours(12));
+            "晚上"
+        }
+    );
+    let time_text = time.format(&format).to_string();
+    let tags = flatten_tags(&illustration["tags"]);
+    let r18 = validate(&tags);
+    let count = illustration["page_count"].as_u64().unwrap_or(0);
+    let view = illustration["total_view"].as_u64().unwrap_or(0);
+    let mark = illustration["total_bookmarks"].as_u64().unwrap_or(0);
+    //build message
     let mut builder = MessageChainBuilder::new().build();
     builder = builder.append(
         format!(
@@ -102,17 +102,6 @@ https://www.pixiv.net/artworks/{id}
 原图:",
         ).parse_text()
     );
-
-
-    let count = 1;
-    let account = "";
-    let title = "";
-    let user_name = "";
-    let time_text = "";
-    let mark = "";
-    let view = "";
-    let r18 = true;
-
     if count == 1 {
         builder = upload_image(event, builder, &id).await.unwrap();
     } else {
