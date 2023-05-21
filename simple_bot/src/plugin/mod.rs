dbe1815f7077f924139e0c0798e1de306d2053fc
LovesAsuna <qq625924077@gmail.com>
2023-05-21 22:58:59 +0800

    :recycle: refactor(plugin): refactor plugin mod

--- simple_bot/src/plugin/mod.rs
+++ simple_bot/src/plugin/mod.rs
@@ -2,6 +2,11 @@ use proc_qq::Module;
 
 mod internal;
 
+pub trait Plugin {
+    fn get_name(&self) -> &str;
+    fn get_desc(&self) -> &str;
+}
+
 pub(crate) fn plugin_entry() -> Vec<Module> {
     internal::plugin_entry()
 }
