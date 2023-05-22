122bb188565493f81b71f9d51f75a2f8b0801ffd
LovesAsuna <qq625924077@gmail.com>
2023-05-22 23:11:54 +0800

    :recycle: refactor(plugin): remove help

--- simple_bot/src/plugin/internal/mod.rs
+++ simple_bot/src/plugin/internal/mod.rs
@@ -1,7 +1,6 @@
 use proc_qq::Module;
 
 mod chat;
-mod help;
 mod information;
 mod picture;
 mod video;
@@ -9,7 +8,6 @@ mod video;
 pub(super) fn plugin_entry() -> Vec<Module> {
     vec![
         chat::module(),
-        help::module(),
         information::module(),
         picture::module(),
         video::module(),
